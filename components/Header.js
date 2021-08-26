8657a016a53a503a1eb719cadb829537bb576aee
wasifraza909 <wasifraza909@gmail.com>
2021-08-26 03:59:50 -0700

    Completed Header component

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,67 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+
+import HeaderIcon from './HeaderIcon';
 
 function Header() {
-    return (
-        <div>
-            <h1>Header</h1>
+  return (
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/* Left */}
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
 
-            {/* Left */}
-            <div>
-                <Image src="https://links.papareact.com/5me" width={40} height={50} layout='fixed' />
-
-
-                <div>
-                    <input type="text" placeholder='Search Facebook' />
-                </div>
-            </div>
-
-            {/* Center */}
-
-            {/* Right */}
-
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
-    )
+      </div>
+
+      {/* Center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} active />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+
+        <p className="font-semibold pr-3 whitespace-nowrap">Sonny Sangha</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;

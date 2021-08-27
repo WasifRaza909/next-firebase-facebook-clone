95406eda2711a94429332c57b5b2f2e92614b739
wasifraza909 <wasifraza909@gmail.com>
2021-08-27 07:29:26 -0700

    Working with Login Component

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
-    "links.papareact.com",
-    "platform-lookaside.fbsbx.com",
-    "firebasestorage.googleapis.com"
+      'links.papareact.com',
+      'platform-lookaside.fbsbx.com',
+      'firebasestorage.googleapis.com',
     ],
   },
 };
